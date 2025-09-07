8560b434924773b21fb4ab334655afde0b58ada5
Pukhraj Motwani <148884916+pm-235779@users.noreply.github.com>
2025-09-07 23:28:33 +0530

    Update pages.js

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -6,7 +6,7 @@ import Leaderboard from "./Leaderboard"
 import Verification from "./Verification"
 import Instructions from "./Instructions"
 import Team from "./Team"
-import ResetPassword from "./ResetPassword.jsx"
+import ResetPassword from "./ResetPassword"
 
 export {
   Home,
